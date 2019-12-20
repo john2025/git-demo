55add7de195e1bfdb5b0c945685b9d24c8dd3ba5
john2025 <ruidezi@163.com>
2019-12-20 11:59:09 +0800

    test change

--- demo-module-1/src/com/test/MainTest.java
+++ demo-module-1/src/com/test/MainTest.java
@@ -6,5 +6,7 @@ package com.test;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
+
+        System.out.println("Test Change");
     }
 }
